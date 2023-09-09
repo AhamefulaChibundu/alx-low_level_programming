208af5c60d7c9588723c018fd2c263d184227642
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-09-09 04:21:12 -0700

    A C program that prints the lowercase alphabet using the while loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lower_case_alphabets = 'a';
+
+	while (lower_case_alphabets <= 'z')
+	{
+		putchar(lower_case_alphabets);
+		lower_case_alphabets++;
+	}
+	putchar('\n');
+	return (0);
+}
