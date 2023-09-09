3737c8bd9b511d445bf9e6f28f0659489c22720b
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-09-09 08:05:56 -0700

    A C program that prints numbers 1-9 with the ascii values

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 48; numbers <= 57; numbers++)
+
+	{
+		printf("%c", numbers);
+	}
+	printf("\n");
+	return (0);
+}
