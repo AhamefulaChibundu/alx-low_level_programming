b1236dcf9d99799f2bbb46adccc4e2c43db9e4a8
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-09-13 12:02:02 -0700

    A function that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Entry point of function
+ *
+ * @num1: first number
+ * @num2: second number
+ *
+ * Description: a function that addsntwo numbers together
+ *
+ * Return: returns sum of numbers
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
