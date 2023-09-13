f3f4c4c611daac16f22bba06fcee5b63be423176
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-09-13 11:11:19 -0700

    A function that prints the 24 hour time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point for function
+ *
+ * Description: prints every minute of the day
+ *
+ */
+
+void jack_bauer(void)
+
+{
+	int mins, hrs;
+
+	for (hrs = 0; hrs <= 23; ++hrs)
+	{
+		for (mins = 0; mins <= 59; ++mins)
+		{
+			_putchar((hrs / 10) + 48);
+			_putchar((hrs % 10) + 48);
+			_putchar(':');
+			_putchar((mins / 10) + 48);
+			_putchar((mins % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
