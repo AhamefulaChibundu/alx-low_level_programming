4fe725cd0fdec7a0d998e4540b5c408b25058ef0
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-09-14 05:51:18 -0700

    A function that prints lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point of function
+ *
+ * Description: prints a line
+ *
+ * @n: epresent the length of `line needed to be printed
+ *
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	for (line = 0; line <= n; line++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
