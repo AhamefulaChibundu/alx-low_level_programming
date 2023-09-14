b7a6501265d1d5be6b2fc8b603bdc8e0cb3804b5
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-09-14 11:28:02 -0700

    A program that prints numbers 1 to 100, fizz for multiple of 3, Buzz for multiple of 5 and FizzBuzz for multiple of both

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints from 1 to 100
+ * prints Fizz if number is a multiple of 3
+ * prints Buzz if number is a multiple of 5
+ * prints FizzBuzz if number is a multiple of both 3 and 5
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 1; numbers <= 100; numbers++)
+	{
+		if (numbers % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (numbers % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (numbers % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", numbers);
+		}
+	}
+	printf("\n");
+	return (0);
+}
