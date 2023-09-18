65131716b11852bb86d5e31642d3a11ab02b7a39
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-09-18 06:48:22 -0700

    A function that updates the value a of a variable which a pointer is pointing to, to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point of function
+ *
+ * Description: updates the value of a pointer to 98
+ *
+ * @n: the pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
