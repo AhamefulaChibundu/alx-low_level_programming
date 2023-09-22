6b0c90bc320f5d7ee7d817f185d036b933e33784
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-09-22 15:23:28 -0700

    Concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,14 +14,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int length = strlen(dest);
-	int m;
+	int index = 0, dest_len = 0;
 
-	for (m = 0; m < n && *src !=  '\0'; m++)
-	{
-		dest[length + m] = src[m];
-		src++;
-	}
-	dest[length + m] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
